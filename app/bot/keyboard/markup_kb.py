80374811b1459d3610b93b5190b5d416e19c3fc1
viss <egor-berezin@bk.ru>
2025-02-10 07:47:28 +0300

    add prev text to kb

--- app/bot/keyboard/markup_kb.py
+++ app/bot/keyboard/markup_kb.py
@@ -7,9 +7,11 @@ del_kbd = ReplyKeyboardRemove()
 
 def main_keyboard(user_role: User.Role) -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Тест")
-    kb.adjust(1)
+    kb.button(text="Мои проекты")
+    kb.button(text="Способы связи")
+    kb.adjust(2)
     if user_role is User.Role.Admin:
-        kb.button(text ='Тест')
-        kb.button(text = 'Тест')
+        kb.button(text ='Добавить проект')
+        kb.button(text = 'Изменить проект')
+        kb.button(text = 'Удалить проект')
     return kb.as_markup(resize_keyboard=True)
