e06b44bde471afde45a10bd166d33b544455c7ae
viss <egor-berezin@bk.ru>
2025-02-10 17:28:12 +0300

    fix admin middleware

--- app/bot/middlewares/is_admin.py
+++ app/bot/middlewares/is_admin.py
@@ -5,22 +5,20 @@ from aiogram.types import Message
 
 from bot.dao import UserDAO
 from bot.models import User
-from dao.database import connection
+from dao.database import async_session_maker
 
-@connection()
 class CheckIsAdmin(BaseMiddleware):
     async def __call__(
         self,
         handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
         event: Message,
         data: Dict[str, Any],
-        session,
-        **kwargs
     ) -> Any:
-        admins_ids = [user.telegram_id for user in await UserDAO.get_admins(session)]
-        if event.from_user.id in admins_ids:
-            return await handler(event, data) 
-        else:
-            await event.answer(
-                "Только администраторы могут пользоваться этим функционалом"
-            )
+        async with async_session_maker() as session:
+            admins_ids = [user.telegram_id for user in await UserDAO.get_admins(session)]
+            if event.from_user.id in admins_ids:
+                return await handler(event, data) 
+            else:
+                await event.answer(
+                    "Только администраторы могут пользоваться этим функционалом"
+                )
