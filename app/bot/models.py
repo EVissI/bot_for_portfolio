9e4210ca400c5a2ad12b86deacd03135ac581dc6
viss <egor-berezin@bk.ru>
2025-02-10 07:14:57 +0300

    init dao for users

---
+++ app/bot/models.py
@@ -0,0 +1,17 @@
+﻿import enum
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger,Enum
+from sqlalchemy.orm import relationship
+
+from typing import Optional
+from dao.database import Base
+
+class User(Base):
+    class Role(enum.Enum):
+        Admin = "Admin"
+        User = "User"
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    username: Mapped[Optional[str]]
+    first_name: Mapped[Optional[str]]
+    last_name: Mapped[Optional[str]]
+    role: Mapped[Role] = mapped_column(Enum(Role), default=Role.User)
