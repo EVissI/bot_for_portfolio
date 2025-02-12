0a5f7466d8b594b598a0c8d32be340b5aee78912
viss <egor-berezin@bk.ru>
2025-02-13 00:13:15 +0300

    add log_error

--- app/config.py
+++ app/config.py
@@ -12,28 +12,33 @@ import redis.asyncio as aioredis
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: SecretStr = ''
-    ADMIN_IDS: List[int] = []
-    BASE_SITE_URL: str = ''
+    BOT_TOKEN: SecretStr
+    ADMIN_IDS: list[int]
+    BASE_SITE_URL: str 
     PORT:int = 8000
     FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
     LOG_ROTATION: str = "10 MB"
     DB_URL: str = 'sqlite+aiosqlite:///app/data/db.sqlite3'
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-    )
+    class Config:
+        case_sensitive = False,
+        env_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     def get_webhook_url(self) -> str:
         """Возвращает URL вебхука с кодированием специальных символов."""
         return f"{self.BASE_SITE_URL}/webhook"
 
 settings = Settings()
-#редис может понадобиться для хранения fsm состояния
+
+
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log.txt")
+logger.add(log_file_path, format=settings.FORMAT_LOG, level="INFO", rotation=settings.LOG_ROTATION)
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log_error.txt")
+logger.add(log_file_path, format=settings.FORMAT_LOG, level="ERROR", rotation=settings.LOG_ROTATION)
+
+    #редис может понадобиться для хранения fsm состояния
 # redis = aioredis.from_url(
 #         settings.redis.url
 #     )
-try:
-    bot = Bot(token=settings.BOT_TOKEN.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher(storage=MemoryStorage())
-except Exception as e:
-    logger.error('Ошибка инициализации бота: ' + str(e))
+bot = Bot(token=settings.BOT_TOKEN.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
 # dp = Dispatcher(storage=RedisStorage(redis))
