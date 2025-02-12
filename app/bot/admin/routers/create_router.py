97bf267e0d8baf48c298b19d739fbe718e7c719e
viss <egor-berezin@bk.ru>
2025-02-12 17:36:20 +0300

    add main admin router to do router tree and pre-done add logic

--- app/bot/admin/routers/create_router.py
+++ app/bot/admin/routers/create_router.py
@@ -1,5 +1,4 @@
-﻿import re
-from aiogram import Router, F
+﻿from aiogram import Router, F
 from aiogram.types import Message,CallbackQuery
 from aiogram.filters import StateFilter
 from aiogram.fsm.state import StatesGroup, State
@@ -11,13 +10,12 @@ from bot.schemas import ProjectModel
 from bot.dao import ProjectDAO
 from bot.models import User
 from bot.keyboard.inline_kb import confirm_kb,AdminCallback
-from bot.keyboard.markup_kb import skip_button,main_keyboard
+from bot.keyboard.markup_kb import cancel_button,main_keyboard
 from dao.database import connection
 
 admin_router_create_project = Router()
 class AddProject(StatesGroup):
     name = State()
-    developers = State()
     small_deskript = State()
     large_deskript = State()
     telegram_bot_link = State()
@@ -27,7 +25,6 @@ class AddProject(StatesGroup):
 
 state_dict = {
     "name": "имя",
-    "developers":"телеграмм тег создателя проекта или телеграмм теги участвующих в проекте через запятую",
     "small_deskript": "краткое описание проекта",
     "large_deskript": "полное описание проекта",
     "telegram_bot_link": "ссылку на бота или его тег через @",
@@ -37,7 +34,7 @@ state_dict = {
 @admin_router_create_project.message(F.text == "Добавить проект")
 async def add_new_project(message: Message, state: FSMContext):
     await state.set_state(AddProject.name)
-    await message.answer("Введите название проекта:", reply_markup=skip_button())
+    await message.answer("Введите название проекта:", reply_markup=cancel_button())
 
 
 async def process_project_data(
@@ -46,22 +43,19 @@ async def process_project_data(
     if message.text.lower() == "отменить создание":
         await state.clear()
         await message.answer(
-            f"Окей отменяю", reply_markup=main_keyboard(User.Role.Admin)
+            f"Окей, отменяю", reply_markup=main_keyboard(User.Role.Admin)
         )
     else:
         await state.update_data({field_name: message.text})
         await state.set_state(next_state)
         await message.answer(
-            f"Введите {state_dict.get(next_state._state)}: ", reply_markup=skip_button()
+            f"Введите {state_dict.get(next_state._state)}: ", reply_markup=cancel_button()
         )
 
 @admin_router_create_project.message(StateFilter(AddProject.name))
 async def process_name(message: Message, state: FSMContext):
-    await process_project_data(message, state, "name", AddProject.developers)
+    await process_project_data(message, state, "name", AddProject.small_deskript)
 
-@admin_router_create_project.message(StateFilter(AddProject.developers))
-async def process_developer(message: Message, state: FSMContext):
-    await process_project_data(message, state, "developers", AddProject.small_deskript)
 
 @admin_router_create_project.message(StateFilter(AddProject.small_deskript))
 async def process_small_deskript(message: Message, state: FSMContext):
@@ -77,11 +71,11 @@ async def process_large_deskript(message: Message, state: FSMContext):
     )
 
 
-telegram_bot_link_pattern = re.compile(r"^(https://t.me/[\w_]+|@[A-Za-z0-9_]+)$")
+telegram_bot_link_pattern = r"^(@[A-Za-z0-9_]{5,32}bot)$"
 
-
+@admin_router_create_project.message(F.text.lower() == "отменить создание")
 @admin_router_create_project.message(
-    F.text.lower() == "отменить создание" or F.text.regexp(telegram_bot_link_pattern),
+    F.text.regexp(telegram_bot_link_pattern),
     StateFilter(AddProject.telegram_bot_link),
 )
 async def process_telegram_bot_link(message: Message, state: FSMContext):
@@ -94,54 +88,46 @@ async def process_telegram_bot_link(message: Message, state: FSMContext):
     ~F.text.regexp(telegram_bot_link_pattern), StateFilter(AddProject.telegram_bot_link)
 )
 async def un_process_telegram_bot_link(message: Message, state: FSMContext):
-    await message.answer("Это не похоже на телеговскую ссылку")
+    await message.answer("Это не похоже на телеговскую ссылку на бота, она должна начинаться на @ и заканчиваться bot")
     await process_project_data(
         message, state, "large_deskript", AddProject.telegram_bot_link
     )
 
 
-https_link_pattern = re.compile(r"^https://.*")
+https_link_pattern = r"^https://.*"
 
 
-async def get_data(state):
-    data = await state.get_data()
-    name, developers, small_deskript, large_deskript, telegram_bot_link, github_link = (
-        data.get('name'),
-        data.get('developers'),
-        data.get('small_deskript'),
-        data.get('large_deskript'),
-        data.get('telegram_bot_link'),
-        data.get('github_link'),
-    )
-    
-    return name,developers,small_deskript,large_deskript,telegram_bot_link,github_link
-
+@admin_router_create_project.message(F.text.lower() == "отменить создание")
 @admin_router_create_project.message(
-    F.text.lower() == "отменить создание" or F.text.regexp(https_link_pattern),
+    F.text.regexp(https_link_pattern),
     StateFilter(AddProject.github_link),
 )
 async def process_github_link(message: Message, state: FSMContext):
     if message.text.lower() == "отменить создание":
         await state.clear()
         await message.answer(
-            f"Окей отменяю", reply_markup=main_keyboard(User.Role.Admin)
+            f"Окей, отменяю", reply_markup=main_keyboard(User.Role.Admin)
         )
-    else:
-        state.update_data({"github_link": message.text})
-    await state.set_state(AddProject.confirm)
-    name, developers, small_deskript, large_deskript, telegram_bot_link, github_link = await get_data(state)
+        return
+    
+    await state.update_data({"github_link": message.text})
 
-    msg = (
-    f"Имя проекта: {name}\n" if name else "" +
-    f"Создатель(и): {developers}\n" if name else "" +
-    f"Краткое описание проекта: {small_deskript}\n" if small_deskript else "" +
-    f"Полное описание проекта: {large_deskript}\n" if large_deskript else "" +
-    f"Ссылка на бота: {telegram_bot_link}\n" if telegram_bot_link else "" +
-    f"Ссылка на репозиторий: {github_link}\n" if github_link else ""
+    data = await state.get_data()
+    logger.info(data)
+    msg = "\n".join(
+    [
+        f"<b>Имя проекта</b>: {data.get('name')}" if data.get('name') else "",
+        f"<b>Краткое описание проекта</b>: {data.get('small_deskript')}" if data.get('small_deskript') else "",
+        f"<b>Полное описание проекта</b>: {data.get('large_deskript')}" if data.get('large_deskript') else "",
+        f"<b>Ссылка на бота</b>: {data.get('telegram_bot_link')}" if data.get('telegram_bot_link') else "",
+        f"<b>Ссылка на репозиторий</b>: {data.get('github_link')}" if data.get('github_link') else ""
+    ]
     )
+    msg = msg.strip()
 
-    await message.answer(msg + "\nПодтвердите публикацию",reply_markup=confirm_kb())
 
+    await message.answer(msg + "\nПодтвердите публикацию",reply_markup=confirm_kb(data['name']))
+    await state.set_state(AddProject.confirm)
 
 @admin_router_create_project.message(
     ~F.text.regexp(https_link_pattern), StateFilter(AddProject.github_link)
@@ -155,17 +141,21 @@ async def un_process_github_link(message: Message, state: FSMContext):
 @admin_router_create_project.callback_query(AdminCallback.filter())
 @connection()
 async def process_confirm(query: CallbackQuery, callback_data: AdminCallback,state:FSMContext, session, **kwargs):
-    if callback_data.action == "admin_confirm_project_yes":
-        name,developers, small_deskript, large_deskript, telegram_bot_link, github_link = await get_data(state)
+    logger.info(callback_data.action)
+    if callback_data.action == "yes":
+        data = await state.get_data()
         values = ProjectModel(
-            name=name,
-            description_small=small_deskript,
-            description_large=large_deskript,
-            telegram_bot_url=telegram_bot_link,
-            developers=developers,
-            github_link=github_link
+            name=data.get("name"),
+            description_small=data.get("small_deskript"),
+            description_large=data.get("large_deskript"),
+            telegram_bot_url=data.get("telegram_bot_link"),
+            github_link=data.get("github_link")
         )
-        
-    elif callback_data.action == "admin_confirm_project_no":
+        await ProjectDAO.add(session,values)
+        await query.message.delete()
+        await query.message.answer('Добавление прошло успешно', reply_markup=main_keyboard(User.Role.Admin))
+        await state.clear()
+    elif callback_data.action == "no":
+        await query.message.delete()
         await query.message.answer('Понял, отменяю публикацию',reply_markup=main_keyboard(User.Role.Admin))
         await state.clear()
--- app/bot/keyboard/inline_kb.py
+++ app/bot/keyboard/inline_kb.py
@@ -1,16 +1,16 @@
-﻿from aiogram.types import InlineKeyboardMarkup
+﻿from typing import Optional
+from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from loguru import logger
 from aiogram.filters.callback_data import CallbackData
 
 
-class AdminCallback(CallbackData, prefix="admin_confirm_project"):
+class AdminCallback(CallbackData, prefix="add_project"):
     action: str
-    project_name:str
-
+    project_name: str
 def confirm_kb(project_name:str) -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
-    logger.debug(AdminCallback.filter())
+
     kb.button(
         text="✅ Подтвердить",
         callback_data=AdminCallback(
@@ -22,7 +22,7 @@ def confirm_kb(project_name:str) -> InlineKeyboardMarkup:
         text="❌ Отклонить",
         callback_data=AdminCallback(
             action="no",
-            project_name = project_name
+            project_name= project_name
         ).pack()
     )
     kb.adjust(2)
